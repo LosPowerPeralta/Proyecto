c14d7fcd95e7033603ade3fe00d2196c4f77bae5
Jorgee18 <101343652+Jorgee18@users.noreply.github.com>
2022-06-02 13:58:09 -0400

    Update main.c

--- main.c
+++ main.c
@@ -6,12 +6,27 @@
 #define UP 0x57
 #define DOWN 0x53
 #define SPACE VK_BACK
-
+/*
+Derecha(D) = 0x44
+izquierda(A) = 0x41
+arriba(W) =   0x57
+abajo(S) = 0x53
+espacio( ) =  VK_BACK
+.\proyecto.exe
+*/
 typedef struct
 {
-  int x;
-  int y;
+  int x, y;
 }Pixel;
+
+Pixel *createCabeza(){
+    Pixel *newCabeza = (Pixel *) malloc(sizeof(Pixel));
+
+    newCabeza->x = 1;
+    newCabeza->y = 1;
+
+    return newCabeza;
+}
 
 void gotoxy(int x, int y)
 {
@@ -48,16 +63,8 @@ void mostrarEscenario()
   printf("=================================================================\n");
 }
 
-Pixel *createCabeza(){
-    Pixel *newCabeza = (Pixel *) malloc(sizeof(Pixel));
-
-    newCabeza->x = 1;
-    newCabeza->y = 1;
-
-    return newCabeza;
-}
-
-void movimiento(Pixel *user) {
+void movimiento(Pixel *user) 
+{
     if (GetAsyncKeyState(LEFT))
         if (user->x != 1) user->x--;
     if (GetAsyncKeyState(RIGTH))
@@ -68,25 +75,133 @@ void movimiento(Pixel *user) {
         if (user->y != 19) user->y++;
 }
 
-int main() {
-    Pixel *cabeza = createCabeza();
-    Pixel *auxCabeza = createCabeza();
-    
-    system("cls");
-    mostrarEscenario();
+void nuevaPartida(Pixel* cabeza)
+{
+  Pixel *auxCabeza = createCabeza();
+  system("cls");
+  mostrarEscenario();
+  gotoxy(cabeza->x, cabeza->y);
+  printf("x");
+
+  while (true) 
+  {
+    Sleep(100);
+    auxCabeza = cabeza;
+    gotoxy(auxCabeza->x, auxCabeza->y);
+    printf(" ");
+    movimiento(cabeza);
     gotoxy(cabeza->x, cabeza->y);
     printf("x");
+  }
 
-    while (true) {
-        Sleep(40);
-        auxCabeza = cabeza;
-        gotoxy(auxCabeza->x, auxCabeza->y);
+}
+/*
+void mapaMovimiento()
+{
+  Pixel pixel;
+  pixel.x = 10;
+  pixel.y = 10;
+
+  gotoxy(pixel.x, pixel.y);
+  printf("*");
+
+  while(1)
+  {
+    //Limite para el ciclo while
+    Sleep(100);
+    //Movimiento izquierda
+    if(GetAsyncKeyState(0x41))
+    {
+      if(pixel.x != 1)
+      {
+        gotoxy(pixel.x,pixel.y);
         printf(" ");
-        movimiento(cabeza);
-        gotoxy(cabeza->x, cabeza->y);
-        printf("x");
+        pixel.x--;
+        gotoxy(pixel.x, pixel.y);
+        printf("*");
+      }
     }
-    
 
-    return EXIT_SUCCESS;
+    //Movimiento derecha
+    if(GetAsyncKeyState(0x44))
+    {
+      if(pixel.x != 63)
+      {
+        gotoxy(pixel.x,pixel.y);
+        printf(" ");
+        pixel.x++;
+        gotoxy(pixel.x, pixel.y);
+        printf("*");
+      }
+    }
+
+    //Movimiento arriba
+    if(GetAsyncKeyState(0x57))
+    {
+      if(pixel.y != 2)
+      {
+        gotoxy(pixel.x,pixel.y);
+        printf(" ");
+        pixel.y--;
+        gotoxy(pixel.x, pixel.y);
+        printf("*");
+      }
+    }
+
+    //Movimiento abajo
+    if(GetAsyncKeyState(0x53))
+    {
+      if(pixel.y != 19)
+      {
+        gotoxy(pixel.x,pixel.y);
+        printf(" ");
+        pixel.y++;
+        gotoxy(pixel.x, pixel.y);
+        printf("*");
+      } 
+    }
+  }
+}
+*/
+
+int main ()
+{
+  Pixel *cabeza = createCabeza();
+  int opcion;
+
+  opcion = 0;
+
+  while(opcion != 12) 
+  {
+    //system("cls");
+
+    printf("\n========== NACHITO BROS ==========\n");
+    printf("(1) NUEVA PARTIDA\n");
+    printf("(2) CARGAR PARTIDA\n");
+    printf("(3) OPCIONES\n");
+    printf("(4) SALIR DEL JUEGO\n");
+
+    printf("\nINGRESE SU OPCION: ");
+    fflush(stdin);
+    scanf("%i", &opcion);
+
+    switch(opcion)
+    {
+      
+      case 1:
+        nuevaPartida(cabeza);
+        break;
+      /*
+      case 2:
+        cargarPartida();
+        break;
+      case 3:
+        opcionesJuego();
+        break;*/
+      case 4:
+        return 0;
+    }
+  }
+
+  return EXIT_SUCCESS;
 }
